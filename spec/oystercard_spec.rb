36d83ab47d04203d7f1bd698663550acd42caa58
James Ransome <jamesransome@msn.com>
2017-07-10 15:52:50 +0100

    can top up oyster card

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,13 +1,21 @@
-require "oystercard"
+require "oyster_card"
 
 describe OysterCard do
-  
-  subject(:oystercard) { described_class.new }
 
-it "will have a balance " do
-	expect(oystercard).to respond_to(:balance)
-	end
+  subject(:oyster_card) { described_class.new }
+  let(:top_up_amount) { 15 }
+
+  it "will have a balance " do
+    expect(oyster_card).to respond_to(:balance)
+  end
+
+  it 'can be topped up' do
+    expect(oyster_card).to respond_to(:top_up).with(1).argument
+  end
+
+  it 'can top up the balance' do
+    expect{ oyster_card.top_up(top_up_amount) }.to change{ oyster_card.balance }.by top_up_amount
+  end
 
 
-
-end	
+end
